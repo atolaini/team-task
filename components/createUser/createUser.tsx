c74c16dbb36c0789257951a5d13dab15207fdb96
atolaini <adam.tolaini@gmail.com>
2023-08-07 15:13:27 +0100

    setting up api and route and some styling changes

--- components/createUser/createUser.tsx
+++ components/createUser/createUser.tsx
@@ -1,11 +1,22 @@
 'use client';
 import { useState } from 'react';
+import { createUser } from '@/utils/api';
 
+import FlexContainer from '../layout/flexContainer';
 import Button from '../ui/button';
 import Modal from '../ui/modal';
+import Form from '../forms/form';
+import Input from '../forms/input';
 
 const CreateUser = () => {
   const [showModal, setShowModal] = useState(false);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  const onSubmitHandler = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+  };
 
   return (
     <div>
@@ -15,7 +26,27 @@ const CreateUser = () => {
       />
       {showModal ? (
         <Modal>
-          <h2>Add a team member</h2>
+          <Form
+            title='Add team member'
+            action='/api/team'
+            onSubmit={onSubmitHandler}
+          >
+            <FlexContainer flexFlow='row'>
+              <Input
+                label='First Name'
+                isRequired={true}
+              />
+              <Input
+                label='Last Name'
+                isRequired={true}
+              />
+            </FlexContainer>
+            <Button
+              btnTxt='Add Team member'
+              onClick={onSubmitHandler}
+              type='submit'
+            />
+          </Form>
         </Modal>
       ) : null}
     </div>
