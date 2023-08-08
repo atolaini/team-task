7739bfe56e2251ce61b9f311a3788ca62086249b
atolaini <adam.tolaini@gmail.com>
2023-08-08 15:21:03 +0100

    branch switch

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+const path = require('path');
 
 module.exports = nextConfig;
 
 module.exports = {
   experimental: {
     serverActions: true,
+  },
+};
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
   },
 };
