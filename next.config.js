674056c794c140bc93ff343b064da3ed2572d65c
atolaini <adam.tolaini@gmail.com>
2023-08-13 16:23:01 +0100

    working on fixing connection issues

--- next.config.js
+++ next.config.js
@@ -5,10 +5,6 @@ const path = require('path');
 module.exports = nextConfig;
 
 module.exports = {
-  experimental: {
-    serverActions: true,
-  },
-
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
