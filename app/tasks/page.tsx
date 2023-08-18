f32505f83025b777787b1a56d75c5371ee46d93f
atolaini <adam.tolaini@gmail.com>
2023-08-18 21:37:27 +0100

    working on tasks page

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,22 +1,18 @@
 import { getUsers } from '@/utils/actions/users.actions';
-import { getTasks } from '@/utils/actions/tasks.actions';
 
 import CreateTask from '@/components/task/createTask';
 
 import TaskContainer from '@/components/task/taskContainer';
 import SideTaskList from '@/components/task/sideTaskList';
 
-const TaskPage = async ({ params }) => {
-  const path = params.path;
-
+const TaskPage = async () => {
   const users = await getUsers();
-  const tasks = await getTasks(path);
 
   return (
     <div>
       <TaskContainer>
         <CreateTask allUsers={users} />
-        <SideTaskList taskData={tasks} />
+        <SideTaskList userTask={users} />
       </TaskContainer>
     </div>
   );
